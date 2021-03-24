db4e8a7c4c830d78dbc8e4f5c13cb055a38486f9
Bruce Alderson <mx@warpedvisions.org>
2021-03-24 15:04:29 -0700

    Adds command line parameters.

--- index.js
+++ index.js
@@ -1,20 +1,50 @@
 #!/usr/bin/env node
+var argv = require('yargs/yargs')(process.argv.slice(2))
+	.usage('Usage: $0 command --file [name] --max [num] --since [unix time] --filter [category]')
+	.command('htmlize', 'Convert links to simple HTML')
+	.default('file', 'data/instapaper-export.csv')
+	.default('max', 150)
+	.default('since', 0)
+	.default('filter', 'unread')
+	.alias('f', 'file')
+	.alias('m', 'max')
+	.alias('F', 'filter')
+	.demandCommand(1)
+	.argv;
 
-const chalk = require('chalk')
 const reader = require('line-reader')
 var csv = require('csv-parse')
 
-const source = {
-	csvFile: "data/instapaper-export.csv",
-	maxEntries: 20,
-	sinceDate: 1611763846,
-	excludeFolders: [],
-	onlyFolder: ''
+// the reading list source
+var source = {
+	csvFile: argv.file,
+	maxEntries: argv.max,
+	sinceDate: argv.since,
+	filter: null,
+	rowLen: 5
 }
 var totalLinks = 0;
 var done = false
 var latestLinkDate;
 var firstLinkDate;
+
+// allow an array of filters
+if (argv.filter) {
+	if (!Array.isArray(argv.filter)) {
+		source.filter = [argv.filter]
+	} else {
+		source.filter = argv.filter
+	}
+}
+
+function isFiltered(value, filters) {
+	if (!filters) return false
+	let found = false
+	filters.every(function(v) {
+		found = (v.toLowerCase() == value.toLowerCase())
+	})
+	return found
+}
 
 
 try {
@@ -22,44 +52,47 @@ try {
 	reader.eachLine(source.csvFile, function(line) {
 		csv(
 			line,
-			{}, 
+			{},
 			function(err, row) {
 				if (row) {
 					row = row[0] // row object is nested
-					
+
 					if (done) return
-					if (!row || row.length != 5) return // skip invalid/incomplete rows
-					
+					if (!row || row.length != source.rowLen) return // skip invalid/incomplete rows
+
 					if (totalLinks >= source.maxEntries) {
 						done = true
 						console.log()
-						console.log(`Links from: ${firstLinkDate} - ${latestLinkDate}`)
+						console.log(`<!-- Links from: ${firstLinkDate} - ${latestLinkDate} (${source.maxEntries} total) -->`)
+						if (source.filter) console.log(`<!-- Filter: ${source.filter} -->`)
 						return
 					}
-					
+
 					// grab link elements from row
-					
+
 					var url = row[0]
 					var title = row[1]
 					var description = row[2]
 					var folder = row[3]
 					var date = row[4]
 
+					folder = folder.toLowerCase()
+
 					// filter
-					
+
 					if (url == 'URL') return // skip header line
-					if (folder == 'Archive') return // skip archived links
-					
+					if (isFiltered(folder, source.filter)) return // skip archived links
+
 					// generate simple HTML output
 
 					totalLinks ++
-		
-					if (description.length > 0) description = ` <p>${description}</p> `
-					var output = `<li data-link-number="${totalLinks}" data-link-date="${date}"><a href="${url}">${title}</a>${description}</li>`
-					
+
+					if (description.length > 0) description = `\n\t<p>${description}</p> `
+					var output = `<li class="${folder}">\n\t<a href="${url}">${title}</a>${description}\n</li>`
+
 					console.log(output) // dump to console to be pipe-able
-					
-					latestLinkDate = date		
+
+					latestLinkDate = date
 					if (!firstLinkDate) firstLinkDate = date
 				}
 			})
